329487ac2958ff3248bf7682e57a585c5eb86e34
Nijaz Muratović <nijazmuratovic10gmail.com>
2025-07-27 13:34:13 +0200

    add tailwind & styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      Hello World!
-    </>
+      <div className="container flex flex-col gap-3">
+        <label htmlFor="jmb">Unesi svoj JMB (jedinstveni matični broj):</label>
+        <input className='bg-white text-black h-14 px-4 rounded' type="number" name="jmb" id="jmb" minLength={13} maxLength={13} required autoFocus />
+        <button id="btn">OK</button>
+        <p id="p"></p>
+    </div>
   )
 }
 
